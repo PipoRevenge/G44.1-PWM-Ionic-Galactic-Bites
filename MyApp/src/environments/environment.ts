5db020ddde731580b592225b972d916bcf174c16
PipoRevenge <alejandrosctrabajos@hotmail.com>
2023-05-13 16:10:19 +0200

    Fixed firebase

--- MyApp/src/environments/environment.ts
+++ MyApp/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'pwm-galactic-bites-48faf',
+    appId: '1:772045363549:web:8c6b312c2c509e7b9370b4',
+    storageBucket: 'pwm-galactic-bites-48faf.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyAL-hdFJx1Ju3ugrCAcjy_TIcaj5r76Rkk',
+    authDomain: 'pwm-galactic-bites-48faf.firebaseapp.com',
+    messagingSenderId: '772045363549',
+    measurementId: 'G-P5YN5D66JG',
+  },
   production: false
 };
 
