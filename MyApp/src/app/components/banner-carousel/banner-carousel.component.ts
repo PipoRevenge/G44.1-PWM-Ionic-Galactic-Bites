859995118011e6ee6f4734de643c1b2931dc50e1
PipoRevenge <alejandrosctrabajos@hotmail.com>
2023-05-21 11:35:17 +0200

    new componets

---
+++ MyApp/src/app/components/banner-carousel/banner-carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-carousel',
+  templateUrl: './banner-carousel.component.html',
+  styleUrls: ['./banner-carousel.component.scss'],
+})
+export class BannerCarouselComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
