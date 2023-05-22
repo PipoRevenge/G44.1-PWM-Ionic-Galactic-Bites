d449c1c76510c459d90dce463f571e5959d91883
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-22 11:06:10 +0100

    added star icon to header

--- MyApp/src/app/components/header/header.component.ts
+++ MyApp/src/app/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit,  ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from 'src/app/services/user/user.service';
+// import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -14,30 +14,32 @@ export class HeaderComponent implements OnInit {
   menuStyle: any;
   offersStyle: any;
 
-  constructor(private router: Router, private userService: UserService) {}
-    userPic: string = "../../../assets/icons/user.png";
-    points: number = 0;
+  userPic: string = "../../../assets/icons/user.png";
+  points: number = 0;
+
+  constructor(private router: Router) {}
+  // constructor(private router: Router, private userService: UserService) {}
 
   ngOnInit(): void {
-      console.log(this.path);
-      if (this.path == '/') this.homeStyle = { color: 'rgb(57, 222, 197)' };
-      if (this.path == '/menu') this.menuStyle = { color: 'rgb(57, 222, 197)' };
-      if (this.path == '/offers') this.offersStyle = { color: 'rgb(57, 222, 197)' };
-        this.checkUser();
-    }
+    console.log(this.path);
+    if (this.path == '/') this.homeStyle = { color: '#39DE9A' };
+    if (this.path == '/menu') this.menuStyle = { color: '#39DE9A' };
+    if (this.path == '/offers') this.offersStyle = { color: '#39DE9A' };
+    // this.checkUser();
+  }
 
-    checkUser(): void {
-        if (this.userService.user) {
-            this.userPic = "../../../assets/profile.jpg";
-            this.points = this.userService.getUserPoints();
-        } else this.userPic =  "../../../assets/icons/user.png";
-    }
+  // checkUser(): void {
+  //     if (this.userService.user) {
+  //         this.userPic = "../../../assets/profile.jpg";
+  //         this.points = this.userService.getUserPoints();
+  //     } else this.userPic =  "../../../assets/icons/user.png";
+  // }
 
-    shoppingCart() {
-        this.router.navigate(['/shopping-cart']);
-    }
+  // shoppingCart() {
+  //     this.router.navigate(['/shopping-cart']);
+  // }
 
-    login() {
-        this.router.navigate(['/profile']);
-    }
+  // login() {
+  //     this.router.navigate(['/profile']);
+  // }
 }
