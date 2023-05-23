bdf70409ee850651dc443bc7cc40f9cc9a1df6f0
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-24 00:40:07 +0100

    scroll to anchor not working

--- MyApp/src/app/pages/offers/offers.page.ts
+++ MyApp/src/app/pages/offers/offers.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.page.html',
   styleUrls: ['./offers.page.scss'],
 })
-export class OffersPage implements OnInit {
+export class OffersPage implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private viewportScroller: ViewportScroller, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    console.log(this.route.snapshot.fragment);
   }
 
+  ngAfterViewInit(): void {
+    this.viewportScroller.scrollToAnchor(this.route.snapshot.fragment);
+  }
 }
