fd07cd63d4eae690f44c735b124230d9329f59d9
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-24 20:25:08 +0100

    fixed login and signup

--- MyApp/src/app/components/login-form/login-form.component.ts
+++ MyApp/src/app/components/login-form/login-form.component.ts
@@ -13,7 +13,7 @@ import Swal from 'sweetalert2';
   	templateUrl: './login-form.component.html',
   	styleUrls: ['./login-form.component.scss']
 })
-export class LoginFormComponent implements Form, AfterViewInit {
+export class LoginFormComponent implements AfterViewInit {
 	loginForm: FormGroup;
 
 	email: HTMLElement;
@@ -32,10 +32,6 @@ export class LoginFormComponent implements Form, AfterViewInit {
 		console.log(this.email);
 		console.log(this.password);
 	}
-
-	onError(input: HTMLElement): void {
-		input.style.boxShadow = '0px 0px 10px rgb(255, 70, 92)';
-	}
 	
 	checkErrors(): boolean {
 		let errors: boolean = false;
@@ -43,70 +39,30 @@ export class LoginFormComponent implements Form, AfterViewInit {
 		const controlEmail = this.loginForm.controls[Object.keys(this.loginForm.controls)[0]];
 		const controlPassword = this.loginForm.controls[Object.keys(this.loginForm.controls)[1]];
 
-		if (controlEmail.errors || controlPassword.errors) {
-			errors = true;
-			if (controlEmail.errors) this.onError(this.email);
-			if (controlPassword.errors) this.onError(this.password);
-		}
+		if (controlEmail.errors || controlPassword.errors) errors = true;
 
 		return errors;
-	}
-
-	resetErrors(): void {
-		document.getElementById('email').style.boxShadow = 'none';
-		document.getElementById('password').style.boxShadow = 'none';
 	}
 
 	signUp(): void {
 		this.router.navigate(['/signup']);
 	}
 
-	async onSubmit(): Promise<void> { 
+	onSubmit(): void { 
 		console.log(this.loginForm.value);
-
-		this.resetErrors();
-		if (!this.checkErrors()) {
+		if (this.checkErrors()) return;
 			
-			let email: string = this.loginForm.value.email;
-			let password: string = this.loginForm.value.password;
-
-			try {
-				await this.userService.login(email, password).then(
-					async (booleano) => {
-						if (booleano) {
-							await Swal.fire({
-								title: '¡Inicio de sesión exitoso!',
-								text: 'Bienvenido de vuelta.',
-								icon: 'success'
-							});
-							this.router.navigate(['/profile']);
-						} else await Swal.fire({
-							title: '¡Opss.. Parece que no estas registrado!',
-							text: 'O te habras equivocado',
-							icon: 'error'
-						});
-					}
-				);
-			
-			} catch (error) {
-				if (!this.userService.emailExists(email)) {
-					console.log('email doesn\'t exists');
-					this.onError(this.email);
-					await Swal.fire({
-						title: 'Error',
-						text: 'El correo electrónico no existe.',
-						icon: 'error'
-					});
-				} else {
-					console.log('password does not match');
-					this.onError(this.password);
-					await Swal.fire({
-						title: 'Error',
-						text: 'La contraseña no coincide.',
-						icon: 'error'
-					});
+		let email: string = this.loginForm.value.email;
+		let password: string = this.loginForm.value.password;
+		try {
+			this.userService.login(email, password).then(
+				async (booleano) => {
+					if (booleano) this.router.navigate(['/profile']);
 				}
-			}
+			);
+		} catch (error) {
+			if (!this.userService.emailExists(email)) console.log('email doesn\'t exists');
+			else console.log('password does not match');
 		}
 	}
 }
