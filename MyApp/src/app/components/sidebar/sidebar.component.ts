8246e712b9326f016253109e00c6b5c249b3907a
Sara González Ramírez <saragonza.lez0608@gmail.com>
2023-05-24 15:43:01 +0100

    menu closes after clicking a page

--- MyApp/src/app/components/sidebar/sidebar.component.ts
+++ MyApp/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { MenuController } from '@ionic/angular'; 
 
 @Component({
   selector: 'app-sidebar',
@@ -12,7 +13,7 @@ export class SidebarComponent  implements OnInit {
   menu: any = { color: '' };
   offers: any = { color: '' };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private menuController: MenuController) {}
 
   ngOnInit(): void {
     this.updateNav();    
@@ -32,6 +33,7 @@ export class SidebarComponent  implements OnInit {
       if (path == '/') this.home = { color: '#39DE9A' };
       else if (path == '/menu') this.menu = { color: '#39DE9A' };
       else if (path == '/offers' || path == '/offers#points' || path == '/offers#discount') this.offers = { color: '#39DE9A' };
+      this.menuController.close();
     });
   }
 
